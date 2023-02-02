4988ad8c12ec64bebe9c2d0c3ddae335f228981e
Eric Julianto <algonacci1@gmail.com>
2023-02-02 19:14:52 +0700

    detail post

--- app.py
+++ app.py
@@ -11,6 +11,13 @@ def root():
     return render_template("index.html", data=data)
 
 
+@app.get("/post/<int:id>")
+def get_post_by_id(id):
+    cursor.execute("SELECT * FROM posts WHERE id = ('{}')".format(id))
+    data = cursor.fetchall()
+    return render_template("post.html", data=data)
+
+
 @app.route("/add_post", methods=["GET", "POST"])
 def add_post():
     if request.method == "GET":
